65210cbba4be50ac0e26816b128864d220e6ac81
Lucas Ventura <lnventura@hotmail.com>
2017-08-06 14:26:36 -0300

    including compatiblity with Docker container

--- app/RedisConnector.js
+++ app/RedisConnector.js
@@ -5,12 +5,15 @@ let redisClient = null;
 
 if (process.env.REDIS_URL) {
     redisClient = redis.createClient(process.env.REDIS_URL, {no_ready_check: true});
-} else {
-    redisClient = redis.createClient(6379, "172.17.0.2");
+} else if (process.env.REDIS_STATISTIC_RECEIVER_NAME) {
+    let port = process.env.REDIS_STATISTIC_RECEIVER_PORT_6379_TCP_PORT;
+    let address = process.env.REDIS_STATISTIC_RECEIVER_PORT_6379_TCP_ADDR;
+    redisClient = redis.createClient(port, address);
 }
 
 redisClient.on('connect', function() {
-    let environment = process.env.REDIS_URL ? 'production' : 'developer';
+    let label_environment = process.env.REDIS_STATISTIC_RECEIVER_PORT_6379_TCP_ADDR ? 'docker production' : 'developer';
+    let environment = process.env.REDIS_URL ? 'heroku production' : label_environment;
     console.log('Redis %s environment connected', environment);
 });
 
